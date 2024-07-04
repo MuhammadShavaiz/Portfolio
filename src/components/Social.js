0f39ff7b471180313e1f3a8a960592ceba76d543
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-07-04 08:38:38 -0700

    initial commit

---
+++ src/components/Social.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/muhammad-shavaiz-sb10/" className="home__social-linkedin" target='_blank'> 
+        <i className='uil uil-dribbble'></i>
+        </a>
+        <a href="https://github.com/MuhammadShavaiz" className="home__social-icon" target='_blank'> 
+        <i className='uil uil-github-alt'></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
