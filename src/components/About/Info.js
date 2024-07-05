3d17e09a568da58347e05ec7c80b6d95e8f85203
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-07-05 08:27:12 -0700

    initial commit

---
+++ src/components/About/Info.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div>
+      <div className="about__info grid">
+        <div className="about__box">
+            <i className="bx bxs-school about__icon"></i>
+            <h3 className="about__title">
+                University
+            </h3>
+            <span className="about__subtitle">
+                NUST
+            </span>
+        </div>
+        <div className="about__box">
+            <i className="bx bxs-briefcase about__icon"></i>
+            <h3 className="about__title">
+                Experience
+            </h3>
+            <span className="about__subtitle">
+                3 Years
+            </span>
+        </div>
+        <div className="about__box">
+        <i class='bx bx-code-block about__icon'></i>
+            <h3 className="about__title">
+                Work
+            </h3>
+            <span className="about__subtitle">
+            Completed 12+ Projects
+            </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Info
