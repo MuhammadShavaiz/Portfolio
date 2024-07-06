84f3e15718f75fa8eebe53c91c220cea6b71ef56
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-07-06 11:19:25 -0700

    initial commit

---
+++ src/components/MachineLearning.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const MachineLearning = () => {
+  return (
+    <div className='"skills__content'>
+    <h3 className="skills__title">
+        Machine Learning Expert
+    </h3>
+    <div className="skills__box">
+      <div className="skills_group">
+          <div className="skills__data">
+              <i className="bx bx-badge-check"></i>
+          
+         <div>
+         <h3 className="skills__name">PyTorch</h3>
+         <span className="skills__level">Intermediate</span>
+         </div>
+          </div>
+
+          <div className="skills__data">
+              <i className="bx bx-badge-check"></i>
+          
+         <div>
+         <h3 className="skills__name">TensorFlow</h3>
+         <span className="skills__level">Intermediate</span>
+         </div>
+          </div>
+
+          <div className="skills__data">
+              <i className="bx bx-badge-check"></i>
+          
+         <div>
+         <h3 className="skills__name">Scikit-learn</h3>
+         <span className="skills__level">Intermediate</span>
+         </div>
+          </div>
+      </div>
+      <div className="skills_group">
+          <div className="skills__data">
+              <i className="bx bx-badge-check"></i>
+          
+         <div>
+         <h3 className="skills__name">Keras</h3>
+         <span className="skills__level">Beginner</span>
+         </div>
+          </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default MachineLearning
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Frontend from '../Frontend'
+import Backend from '../Backend'
+import MachineLearning from '../MachineLearning'
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+         <h2 className="section__title">Skills</h2>
+        <span className="section__subtitle">
+            My Technical Level
+        </span>
+
+        <div className="skills_container container grid">
+            <Frontend/>
+            <Backend/>
+            <MachineLearning/>
+        </div>
+    </section>
+  )
+}
+
+export default Skills
