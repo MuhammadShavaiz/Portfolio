d1893727733c3b846657adb94dba061efdf82d23
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-07-07 04:30:23 -0700

    initial commit

--- src/components/Qualification/Qualification.js
+++ src/components/Qualification/Qualification.js
@@ -1,197 +1,120 @@
-import React from 'react'
-import"./Qualification.css"
-const qualification = () => {
+import React, { useState } from 'react';
+import "./Qualification.css";
+
+const Qualification = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
   return (
     <section className="qualification section">
-        <h2 className="section__title">qualification</h2>
-        <span className="section__subtitle">
-            My Personel Journey
-        </span>
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">My Personal Journey</span>
 
-        <div className="qualification__container container">
-            <div className="qualification__tabs">
-                <div className="qualification__button button--flex">
-                    <i className="uil uil-graduation-cap
-                    qualification__icon"></i>{" "} Education
-                </div>
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div
+            className={toggleState === 1 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}
+            onClick={() => toggleTab(1)} // Corrected onClick attribute
+          >
+            <i className="uil uil-graduation-cap qualification__icon"></i>{" "} Education
+          </div>
 
-                <div className="qualification__button button--flex">
-                    <i className="uil uil-briefcase-alt
-                    qualification__icon"></i>{" "} Experience
-                </div>
-            </div>
-
-            <div className="qualification__sections">
-                <div className="qualification__content qualification__content-active">
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">Web Design</h3>
-                            <span className='qualificaiton__subtitle'>
-                                NUST
-                            </span>
-                            <div className="qualifficaiton__calender">
-                                <i className="uil uil-calendar-alt"></i>
-                                2021 - Present
-                            </div>
-                        </div>
-
-                        <div>
-                            <span className="qualification__rounder">
-                            </span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-
-                    <div className="qualification__data">
-                        <div></div>
-
-                        <div>
-                            <span className="qualification__rounder">
-                            </span>
-                            <span className="qualification__line"></span>
-                        </div>
-                        <div>
-                            <h3 className="qualification__title">Art Director</h3>
-                            <span className='qualificaiton__subtitle'>
-                                NUST
-                            </span>
-                            <div className="qualifficaiton__calender">
-                                <i className="uil uil-calendar-alt"></i>
-                                2021 - Present
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">machine learn</h3>
-                            <span className='qualificaiton__subtitle'>
-                                NUST
-                            </span>
-                            <div className="qualifficaiton__calender">
-                                <i className="uil uil-calendar-alt"></i>
-                                2021 - Present
-                            </div>
-                        </div>
-
-                        <div>
-                            <span className="qualification__rounder">
-                            </span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-
-                    <div className="qualification__data">
-                        <div></div>
-
-                        <div>
-                            <span className="qualification__rounder">
-                            </span>
-                            <span className="qualification__line"></span>
-                        </div>
-                        <div>
-                            <h3 className="qualification__title">deep learn</h3>
-                            <span className='qualificaiton__subtitle'>
-                                NUST
-                            </span>
-                            <div className="qualifficaiton__calender">
-                                <i className="uil uil-calendar-alt"></i>
-                                2021 - Present
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div className="qualification__content">
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">Web Design</h3>
-                            <span className='qualificaiton__subtitle'>
-                                NUST
-                            </span>
-                            <div className="qualifficaiton__calender">
-                                <i className="uil uil-calendar-alt"></i>
-                                2021 - Present
-                            </div>
-                        </div>
-
-                        <div>
-                            <span className="qualification__rounder">
-                            </span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-
-                    <div className="qualification__data">
-                        <div></div>
-
-                        <div>
-                            <span className="qualification__rounder">
-                            </span>
-                            <span className="qualification__line"></span>
-                        </div>
-                        <div>
-                            <h3 className="qualification__title">Art Director</h3>
-                            <span className='qualificaiton__subtitle'>
-                                NUST
-                            </span>
-                            <div className="qualifficaiton__calender">
-                                <i className="uil uil-calendar-alt"></i>
-                                2021 - Present
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">machine learn</h3>
-                            <span className='qualificaiton__subtitle'>
-                                NUST
-                            </span>
-                            <div className="qualifficaiton__calender">
-                                <i className="uil uil-calendar-alt"></i>
-                                2021 - Present
-                            </div>
-                        </div>
-
-                        <div>
-                            <span className="qualification__rounder">
-                            </span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-
-                    <div className="qualification__data">
-                        <div></div>
-
-                        <div>
-                            <span className="qualification__rounder">
-                            </span>
-                            <span className="qualification__line"></span>
-                        </div>
-                        <div>
-                            <h3 className="qualification__title">deep learn</h3>
-                            <span className='qualificaiton__subtitle'>
-                                NUST
-                            </span>
-                            <div className="qualifficaiton__calender">
-                                <i className="uil uil-calendar-alt"></i>
-                                2021 - Present
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
+          <div
+            className={toggleState === 2 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}
+            onClick={() => toggleTab(2)} // Corrected onClick attribute
+          >
+            <i className="uil uil-briefcase-alt qualification__icon"></i>{" "} Experience
+          </div>
         </div>
-    </section>
-  )
-}
 
-export default qualification
+        <div className="qualification__sections">
+          <div className={toggleState === 1 ? "qualification__content qualification__content-active" : "qualification__content"}>
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Bachelors CS</h3>
+                <span className="qualification__subtitle">NUST</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">HSSC</h3>
+                <span className="qualification__subtitle">GCU</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2018 - 2020
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">SSC</h3>
+                <span className="qualification__subtitle">APS</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2016 - 2018
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+          </div>
+
+          <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Machine Learning Fellow</h3>
+                <span className="qualification__subtitle">ByteWise Limited</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> june 2024 - august 2024
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">Game Programmer</h3>
+                <span className="qualification__subtitle">Mindstorm Studios</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> june 2023 - august 2023
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
