435eb0547b49335ebde4596cf313b71418da94d7
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-07-08 08:13:45 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Qualification from './components/Qualification/Qualification';
+import Contact from  './components/Contact/Contact';
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
       <About/>
       <Skills/>
       <Qualification/>
+      <Contact/>
     </main>
     </>
   );
