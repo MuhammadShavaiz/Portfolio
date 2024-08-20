b34516a73efd224af6793562810c38359f53e4e7
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-08-20 11:11:29 -0700

    initial commit

--- src/components/Projects/Data.js
+++ src/components/Projects/Data.js
@@ -1,6 +1,8 @@
 import Work1 from '../../assets/work1.jpg'
 import Work2 from '../../assets/work2.jpg'
 import Work3 from '../../assets/work3.jpg'
+import Work4 from '../../assets/work4.jpg'
+import Work5 from '../../assets/work5.jpg'
 
 export const projectsData = [
     {
@@ -12,9 +14,27 @@ export const projectsData = [
     {
         id: 2,
         image: Work2,
-        title: 'Machine/Deep learning',
+        title: 'AI Visual Q/A',
         category: 'AI/ML'
-    }
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: 'CLothes Generator GANS',
+        category: 'AI/ML'
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: 'Automated Lane Detection',
+        category: 'AI/ML'
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: 'Hand Sketch Recognizer',
+        category: 'AI/ML'
+    },
 ];
     
 export const projectsNav = [
