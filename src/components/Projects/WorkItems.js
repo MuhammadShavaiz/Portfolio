04649460d54c14e0f958ef6d07a1fe08a3767dd2
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-08-20 11:40:12 -0700

    initial commit

--- src/components/Projects/WorkItems.js
+++ src/components/Projects/WorkItems.js
@@ -7,10 +7,10 @@ const WorkItems = ({item}) => {
         <h3 className="work__title">
             {item.title}
         </h3>
-        <a href="#" className="work__button">
+        {/*<a href="#" className="work__button">
             Demo <i className="bx bx-right-arrow-alt work__button-icon
             "></i>
-        </a>
+        </a>*/}
       </div>
   )
 }
