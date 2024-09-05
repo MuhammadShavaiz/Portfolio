ce8b238be089d3d7c6c5d31555afc6d1620935ed
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-09-05 12:00:21 -0700

    added some webwork

--- src/components/Projects/Data.js
+++ src/components/Projects/Data.js
@@ -3,6 +3,9 @@ import Work2 from '../../assets/work2.jpg'
 import Work3 from '../../assets/work3.jpg'
 import Work4 from '../../assets/work4.jpg'
 import Work5 from '../../assets/work5.jpg'
+import Work6 from '../../assets/Work6.jpg'
+import Work7 from '../../assets/work7.jpg'
+import Work7 from '../../assets/work8.jpg'
 
 export const projectsData = [
     {
@@ -34,6 +37,24 @@ export const projectsData = [
         image: Work5,
         title: 'Hand Sketch Recognizer',
         category: 'AI/ML'
+    },
+    {
+        id: 6,
+        image: Work6,
+        title: 'Notes App',
+        category: 'web'
+    },
+    {
+        id: 7,
+        image: Work7,
+        title: 'Word Utility App',
+        category: 'web'
+    },
+    {
+        id: 8,
+        image: Work8,
+        title: 'Ecommerce App',
+        category: 'web'
     },
 ];
     
