2ca3d7db6908a1dde430c34aa868a082d33afeea
Muhammad Shavaiz <shavaizsohail@gmail.com>
2024-09-06 12:57:18 -0700

    contact backend added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "./Contact.css";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_ypiac3g', 'template_nk8go3p', form.current, {
+        publicKey: '9H0uxiS9eEhv7NaRv',
+      })
+      e.target.reset();
+  };
   return (
     <section className="contact section" id="contact">
       <h2 className="section__title">Get in touch</h2>
@@ -55,7 +67,7 @@ const Contact = () => {
         <div className="contact__content">
           <h3 className="contact__title">Write me your project</h3>
 
-          <form className="contact__form">
+          <form ref={form} onSubmit={sendEmail} className="contact__form">
             <div className="contact__form-div">
               <label className="contact__form-tag">Name</label>
               <input type="text" name="name" className="contact__form-input" placeholder="Insert your name" />
